d3db89b22fe26f796a0f6212a61612b4b242609a
Jake Wengroff <j@jxb1.com>
2015-12-05 23:11:16 -0500

    Converted from ActiveRecord to Mongoid.
    Installed MongoDB and Mongoid. Generated new Model and Controller for Products. Removed ActiveRecord errors.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
-class Product < ActiveRecord::Base
+class Product
+  include Mongoid::Document
+  field :name, type: String
+  field :type, type: String
+  field :length, type: Integer
+  field :width, type: Integer
+  field :height, type: Integer
+  field :weight, type: Integer
 end
