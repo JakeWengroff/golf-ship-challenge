66a3d58ff589f670b8a27d76c95ff2517230857f
Jake Wengroff <j@jxb1.com>
2015-12-07 15:19:22 -0500

    Added logic to controller for queries.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,9 +9,24 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    if @product.save
-      redirect_to products_path, notice: "The product has been created!" and return
+    if (@product.type == "Golf") && (56 >= @product.length > 52)
+      @product.name = "Extra Large Package"
+    elsif (@product.type == "Golf") && (52 >= @product.length > 48)
+      @product.name = "Large Package"
+
+    elsif (@product.type == "Luggage") && (41 >= @product.length > 26)
+      @product.name = "Oversized Bag"
+    elsif (@product.type == "Luggage") && (22 >= @product.height > 7)
+      @product.name = "Checked Bag"
+
+    elsif (@product.type == "Ski") && (10 >= @product.width >= 8) && (40 >= @product.weight > 25)
+      @product.name = "Double Ski Bag"
+    elsif (@product.type == "Ski") && (@product.width == 14) && (40 >= @product.weight > 25)
+      @product.name = "Double Snowboard Bag"
     end
+
+    @product.save
+      redirect_to products_path, notice: "The product has been created!" and return
     render 'new'
   end
 
